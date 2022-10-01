e8922ae46eb8618bb87de937ca47ea30342a59c7
Ritesh Kumar <89743011+55ritesh@users.noreply.github.com>
2022-10-01 23:54:47 +0530

    Update dp.cpp

--- dp.cpp
+++ dp.cpp
@@ -1,1 +1,63 @@
-HWJYMNJ6 J JQ
+## 0 - 1 Knapsack Problem
+
+
+You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
+In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, 
+find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item or dont pick it (0-1 property).
+  
+  
+  #include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       int dp[n+1][W+1];
+    
+       for(int i=0;i<=n;i++){
+           for(int j=0;j<=W;j++){
+               if(i==0||j==0)
+               dp[i][j]=0;
+               else if(wt[i-1]<=j){
+                   dp[i][j]=max(dp[i-1][j],dp[i-1][j-wt[i-1]]+val[i-1]);
+               }
+               else
+               dp[i][j]=dp[i-1][j];
+           }
+       }
+          return dp[n][W];
+       }
+};
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}
